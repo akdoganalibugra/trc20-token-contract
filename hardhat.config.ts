d51ae2559921ff00fc2b5610100393f55060a3ef
akdoganalibugra1 <akdoganalibugra@gmail.com>
2024-08-26 18:25:45 +0300

    feat: Add Tron network configuration to hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+    solidity: "0.8.24",
+    networks: {
+        tron: {
+            url: process.env.TRON_RPC_URL,
+            accounts: [process.env.DEPLOYER_PRIVATE_KEY || ""],
+        },
+    },
 };
 
 export default config;
